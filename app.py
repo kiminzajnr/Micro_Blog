34687dd50ab54f67646fff5088f8c8b165957a4c
kiminzajnr <kiminzaeric@gmail.com>
2024-08-20 18:13:25 +0300

    Display entries

--- app.py
+++ app.py
@@ -5,10 +5,13 @@ from flask import Flask, render_template, request
 app = Flask(__name__)
 
 
+entries = []
+
+
 @app.route("/", methods=["GET", "POST"])
 def home():
     if request.method == "POST":
         entry_content = request.form.get("content")
         formatted_date = datetime.datetime.today().strftime("%Y-%m-%d")
-        print(entry_content, formatted_date)
-    return render_template("home.html")
+        entries.append((entry_content, formatted_date))
+    return render_template("home.html", entries=entries)
